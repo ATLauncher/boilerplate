4543cf9571dbc53f8f63f34c3a1bcc3d888a9234
Ryan Dowling <ryan@ryandowling.me>
2018-04-01 20:35:42 +1000

    feat: add in label syncing for ATLauncher repositories

--- README.md
+++ README.md
@@ -4,6 +4,40 @@ This is the meta repository for ATLauncher.
 
 ## Use
 
-This repository is used for discussing ATLauncher issues that aren't directly related to a single
-repository as well as housing the boilerplate used for our repositories (such as github issue
-templates and code of conduct).
+This repository is used for discussing ATLauncher issues that aren't directly related to a single repository as well as
+housing the boilerplate used for our repositories (such as github issue templates and code of conduct).
+
+## Scripts
+
+This repository also contains some housekeeping scripts which can be used to keep ATLauncher repositories up to date
+with things such as changes to labels, base files among other things.
+
+### Setup
+
+Before you begin you need to make sure you have the NodeJS 8 or newer installed on your computer.
+
+We use and recommend `nvm` for ease of use when working in this (and other) repositories. `nvm` will allow you to run
+`nvm use` in a projects directory to automatically use the version of NodeJS they recommend for their project.
+
+You can download `nvm` from the below repositories:
+
+* Windows: <https://github.com/coreybutler/nvm-windows>
+* OSX/\*nix: <https://github.com/creationix/nvm>
+
+Next install the project dependencies:
+
+```bash
+npm install
+```
+
+Lastly you will need to either set an environment variable called `GITHUB_ACCESS_TOKEN` with a GitHub access token
+generated from <https://github.com/settings/tokens>.
+
+### Sync GitHub Labels
+
+If labels are updated in the `github.json` file, then those labels can be synced to all repositories listed in
+`github.json` by running:
+
+```bash
+npm run github:sync-labels
+```
---
+++ src/github/sync-labels.js
@@ -0,0 +1,95 @@
+const octokitRest = require('@octokit/rest');
+
+const github = require('../../github.json');
+
+const octokit = octokitRest({
+    requestMedia: 'application/vnd.github.v3+json',
+    headers: {
+        'user-agent': 'octokit/rest.js v15.2.6'
+    },
+});
+
+if (!process.env.GITHUB_ACCESS_TOKEN) {
+    console.error('GITHUB_ACCESS_TOKEN environment variable must be provided.');
+    process.exit(1);
+}
+
+octokit.authenticate({
+    type: 'token',
+    token: process.env.GITHUB_ACCESS_TOKEN
+})
+
+github.repositories.forEach(async ({ owner, repo }) => {
+    console.log(`Updating labels for project ${owner}/${repo}`);
+
+    try {
+        const { data: remoteLabels } = await octokit.issues.getLabels({
+            owner,
+            repo,
+            per_page: 100,
+            headers: {
+                accept: 'application/vnd.github.symmetra-preview+json'
+            }
+        });
+
+        remoteLabels.forEach(async (remoteLabel) => {
+            const localLabel = github.labels.find(({ name }) => name.toLowerCase() === remoteLabel.name.toLowerCase());
+
+            if (!localLabel) {
+                console.warn(`[${owner}/${repo}] Deleting remote label ${remoteLabel.name}`);
+                await octokit.issues.deleteLabel({ owner, repo, name: remoteLabel.name })
+                return;
+            }
+
+            const remoteLabelOutOfDate = (
+                remoteLabel.name !== localLabel.name ||
+                remoteLabel.description !== localLabel.description ||
+                remoteLabel.color !== localLabel.color
+            );
+
+            if (!remoteLabelOutOfDate) {
+                console.log(`[${owner}/${repo}] Remote label ${remoteLabel.name} already up to date`);
+                return;
+            }
+
+            console.log(`[${owner}/${repo}] Updating remote label ${localLabel.name}`);
+
+            await octokit.issues.updateLabel({
+                owner,
+                repo,
+                oldname: remoteLabel.name,
+                name: localLabel.name,
+                color: localLabel.color,
+                description: localLabel.description,
+                headers: {
+                    accept: 'application/vnd.github.symmetra-preview+json'
+                }
+            });
+        });
+
+        const missingLocalLabels = github.labels.filter((localLabel) => (
+            !remoteLabels.find((remoteLabel) => remoteLabel.name === localLabel.name)
+        ));
+
+        missingLocalLabels.forEach(async (localLabel) => {
+            console.log(`[${owner}/${repo}] Adding remote label ${localLabel.name}`);
+
+            await octokit.issues.createLabel({
+                owner,
+                repo,
+                name: localLabel.name,
+                color: localLabel.color,
+                description: localLabel.description,
+                headers: {
+                    accept: 'application/vnd.github.symmetra-preview+json'
+                }
+            });
+        });
+    } catch (e) {
+        console.log('-----');
+        console.error(`[${owner}/${repo}] Error from GitHub:`)
+        console.log();
+        console.error(e);
+        process.exit(1);
+    }
+});
