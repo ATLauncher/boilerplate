1008e91b9f7e5d14dba19364cb6e640674529602
Ryan Dowling <ryan@ryandowling.me>
2019-05-24 21:58:57 +1000

    fix: issues with updated @octokit/rest api

--- src/github/sync-labels.js
+++ src/github/sync-labels.js
@@ -1,54 +1,49 @@
-const octokitRest = require('@octokit/rest');
+const Octokit = require('@octokit/rest');
 
 const github = require('../../github.json');
-
-const octokit = octokitRest({
-    requestMedia: 'application/vnd.github.v3+json',
-    headers: {
-        'user-agent': 'octokit/rest.js v15.2.6'
-    },
-});
 
 if (!process.env.ACCESS_TOKEN_GITHUB) {
     console.error('ACCESS_TOKEN_GITHUB environment variable must be provided.');
     process.exit(1);
 }
 
-octokit.authenticate({
-    type: 'token',
-    token: process.env.ACCESS_TOKEN_GITHUB
-})
+const octokit = Octokit({
+    requestMedia: 'application/vnd.github.v3+json',
+    userAgent: 'octokit/rest.js v16.27.0',
+    previews: ['symmetra-preview'],
+    auth: `token ${process.env.ACCESS_TOKEN_GITHUB}`,
+});
 
 github.repositories.forEach(async ({ owner, repo }) => {
     console.log(`Updating labels for project ${owner}/${repo}`);
 
     try {
-        const { data: remoteLabels } = await octokit.issues.getLabels({
+        const { data: remoteLabels } = await octokit.issues.listLabelsForRepo({
             owner,
             repo,
             per_page: 100,
-            headers: {
-                accept: 'application/vnd.github.symmetra-preview+json'
-            }
         });
 
-        remoteLabels.forEach(async (remoteLabel) => {
-            const localLabel = github.labels.find(({ name }) => name.toLowerCase() === remoteLabel.name.toLowerCase());
+        remoteLabels.forEach(async remoteLabel => {
+            const localLabel = github.labels.find(
+                ({ name }) => name.toLowerCase() === remoteLabel.name.toLowerCase(),
+            );
 
             if (!localLabel) {
                 console.warn(`[${owner}/${repo}] Deleting remote label ${remoteLabel.name}`);
-                await octokit.issues.deleteLabel({ owner, repo, name: remoteLabel.name })
+                await octokit.issues.deleteLabel({ owner, repo, name: remoteLabel.name });
                 return;
             }
 
-            const remoteLabelOutOfDate = (
+            const remoteLabelOutOfDate =
                 remoteLabel.name !== localLabel.name ||
                 remoteLabel.description !== localLabel.description ||
-                remoteLabel.color !== localLabel.color
-            );
+                remoteLabel.color !== localLabel.color;
 
             if (!remoteLabelOutOfDate) {
-                console.log(`[${owner}/${repo}] Remote label ${remoteLabel.name} already up to date`);
+                console.log(
+                    `[${owner}/${repo}] Remote label ${remoteLabel.name} already up to date`,
+                );
                 return;
             }
 
@@ -57,21 +52,18 @@ github.repositories.forEach(async ({ owner, repo }) => {
             await octokit.issues.updateLabel({
                 owner,
                 repo,
-                oldname: remoteLabel.name,
+                current_name: remoteLabel.name,
                 name: localLabel.name,
                 color: localLabel.color,
                 description: localLabel.description,
-                headers: {
-                    accept: 'application/vnd.github.symmetra-preview+json'
-                }
             });
         });
 
-        const missingLocalLabels = github.labels.filter((localLabel) => (
-            !remoteLabels.find((remoteLabel) => remoteLabel.name === localLabel.name)
-        ));
+        const missingLocalLabels = github.labels.filter(
+            localLabel => !remoteLabels.find(remoteLabel => remoteLabel.name === localLabel.name),
+        );
 
-        missingLocalLabels.forEach(async (localLabel) => {
+        missingLocalLabels.forEach(async localLabel => {
             console.log(`[${owner}/${repo}] Adding remote label ${localLabel.name}`);
 
             await octokit.issues.createLabel({
@@ -80,14 +72,11 @@ github.repositories.forEach(async ({ owner, repo }) => {
                 name: localLabel.name,
                 color: localLabel.color,
                 description: localLabel.description,
-                headers: {
-                    accept: 'application/vnd.github.symmetra-preview+json'
-                }
             });
         });
     } catch (e) {
         console.log('-----');
-        console.error(`[${owner}/${repo}] Error from GitHub:`)
+        console.error(`[${owner}/${repo}] Error from GitHub:`);
         console.log();
         console.error(e);
         process.exit(1);
--- src/github/update-template-files.js
+++ src/github/update-template-files.js
@@ -5,143 +5,147 @@ const sha1 = require('sha1');
 const base64 = require('base-64');
 const { DateTime } = require('luxon');
 const { createHash } = require('crypto');
-const octokitRest = require('@octokit/rest');
+const Octokit = require('@octokit/rest');
 
 const github = require('../../github.json');
-
-const octokit = octokitRest({
-    requestMedia: 'application/vnd.github.v3+json',
-    headers: {
-        'user-agent': 'octokit/rest.js v15.2.6',
-    },
-});
 
 if (!process.env.ACCESS_TOKEN_GITHUB) {
     console.error('ACCESS_TOKEN_GITHUB environment variable must be provided.');
     process.exit(1);
 }
 
-octokit.authenticate({
-    type: 'token',
-    token: process.env.ACCESS_TOKEN_GITHUB,
+const octokit = Octokit({
+    requestMedia: 'application/vnd.github.v3+json',
+    userAgent: 'octokit/rest.js v16.27.0',
+    auth: `token ${process.env.ACCESS_TOKEN_GITHUB}`,
 });
 
-github.repositories.filter(({ updateTemplateFiles }) => updateTemplateFiles).forEach(async ({ owner, repo, excludedTemplateFiles = [] }) => {
-    console.log(`Updating template files for project ${owner}/${repo}`);
+github.repositories
+    .filter(({ updateTemplateFiles }) => updateTemplateFiles)
+    .forEach(async ({ owner, repo, excludedTemplateFiles = [] }) => {
+        console.log(`Updating template files for project ${owner}/${repo}`);
 
-    try {
-        const { data: repoData } = await octokit.repos.get({ owner, repo });
+        try {
+            const { data: repoData } = await octokit.repos.get({ owner, repo });
 
-        // get the year the repository was created
-        const { year: yearCreated } = DateTime.fromISO(repoData.created_at);
+            // get the year the repository was created
+            const { year: yearCreated } = DateTime.fromISO(repoData.created_at);
 
-        const templateMetaData = {
-            year: yearCreated,
-        };
+            const templateMetaData = {
+                year: yearCreated,
+            };
 
-        github.templateFiles.filter((file) => !excludedTemplateFiles.includes(file)).forEach(async (templateFile) => {
-            const template = _.template(fs.readFileSync(path.resolve(__dirname, '../../template', templateFile)));
+            github.templateFiles
+                .filter(file => !excludedTemplateFiles.includes(file))
+                .forEach(async templateFile => {
+                    const template = _.template(
+                        fs.readFileSync(path.resolve(__dirname, '../../template', templateFile)),
+                    );
 
-            const updatedFileData = template(templateMetaData);
-            const updatedBase64data = base64.encode(updatedFileData);
-            const updatedFileSha = sha1(updatedFileData);
+                    const updatedFileData = template(templateMetaData);
+                    const updatedBase64data = base64.encode(updatedFileData);
+                    const updatedFileSha = sha1(updatedFileData);
 
-            try {
-                const { data: existingFile } = await octokit.repos.getContent({
-                    owner,
-                    repo,
-                    path: templateFile,
-                    ref: 'master',
+                    try {
+                        const { data: existingFile } = await octokit.repos.getContents({
+                            owner,
+                            repo,
+                            path: templateFile,
+                            ref: 'master',
+                        });
+
+                        const oldFileData = base64.decode(existingFile.content);
+                        const oldBase64data = base64.encode(oldFileData);
+                        const oldFileSha = sha1(oldFileData);
+
+                        const fileUpToDate = _.isEqual(oldFileData, updatedFileData);
+
+                        if (fileUpToDate) {
+                            console.log(
+                                `File '${templateFile}' already up to date for project ${owner}/${repo}`,
+                            );
+                            return;
+                        }
+
+                        console.log(`Updating '${templateFile}' for project ${owner}/${repo}`);
+
+                        await octokit.repos.updateFile({
+                            owner,
+                            repo,
+                            path: templateFile,
+                            message: `chore: update ${templateFile}`,
+                            content: updatedBase64data,
+                            sha: existingFile.sha,
+                            committer: {
+                                name: 'ATLauncher Bot',
+                                email: '38805255+atlauncher-bot@users.noreply.github.com',
+                            },
+                            branch: 'master',
+                        });
+                    } catch (e) {
+                        if (e.code !== 404) {
+                            throw e;
+                            return;
+                        }
+
+                        console.log(`Creating file '${templateFile}' for project ${owner}/${repo}`);
+
+                        await octokit.repos.createFile({
+                            owner,
+                            repo,
+                            path: templateFile,
+                            message: `chore: add ${templateFile}`,
+                            content: updatedBase64data,
+                            committer: {
+                                name: 'ATLauncher Bot',
+                                email: '+atlauncher-bot@users.noreply.github.com',
+                            },
+                            branch: 'master',
+                        });
+
+                        return;
+                    }
                 });
 
-                const oldFileData = base64.decode(existingFile.content);
-                const oldBase64data = base64.encode(oldFileData);
-                const oldFileSha = sha1(oldFileData);
+            github.oldTemplateFiles.forEach(async templateFile => {
+                try {
+                    const { data: existingFile } = await octokit.repos.getContents({
+                        owner,
+                        repo,
+                        path: templateFile,
+                        ref: 'master',
+                    });
 
-                const fileUpToDate = _.isEqual(oldFileData, updatedFileData);
+                    console.log(`Deleting file '${templateFile}' for project ${owner}/${repo}`);
 
-                if (fileUpToDate) {
-                    console.log(`File '${templateFile}' already up to date for project ${owner}/${repo}`);
-                    return;
-                }
+                    await octokit.repos.deleteFile({
+                        owner,
+                        repo,
+                        path: templateFile,
+                        message: `chore: delete ${templateFile}`,
+                        sha: existingFile.sha,
+                        committer: {
+                            name: 'ATLauncher Bot',
+                            email: '+atlauncher-bot@users.noreply.github.com',
+                        },
+                        branch: 'master',
+                    });
+                } catch (e) {
+                    if (e.code === 404) {
+                        console.log(
+                            `File '${templateFile}' already deleted for project ${owner}/${repo}`,
+                        );
+                        return;
+                    }
 
-                console.log(`Updating '${templateFile}' for project ${owner}/${repo}`);
-
-                await octokit.repos.updateFile({
-                    owner,
-                    repo,
-                    path: templateFile,
-                    message: `chore: update ${templateFile}`,
-                    content: updatedBase64data,
-                    sha: existingFile.sha,
-                    committer: {
-                        name: 'ATLauncher Bot',
-                        email: '38805255+atlauncher-bot@users.noreply.github.com',
-                    },
-                    branch: 'master',
-                });
-            } catch (e) {
-                if (e.code !== 404) {
                     throw e;
-                    return;
                 }
-
-                console.log(`Creating file '${templateFile}' for project ${owner}/${repo}`);
-
-                await octokit.repos.createFile({
-                    owner,
-                    repo,
-                    path: templateFile,
-                    message: `chore: add ${templateFile}`,
-                    content: updatedBase64data,
-                    committer: {
-                        name: 'ATLauncher Bot',
-                        email: '+atlauncher-bot@users.noreply.github.com',
-                    },
-                    branch: 'master',
-                });
-
-                return;
-            }
-        });
-
-        github.oldTemplateFiles.forEach(async (templateFile) => {
-            try {
-                const { data: existingFile } = await octokit.repos.getContent({
-                    owner,
-                    repo,
-                    path: templateFile,
-                    ref: 'master',
-                });
-
-                console.log(`Deleting file '${templateFile}' for project ${owner}/${repo}`);
-
-                await octokit.repos.deleteFile({
-                    owner,
-                    repo,
-                    path: templateFile,
-                    message: `chore: delete ${templateFile}`,
-                    sha: existingFile.sha,
-                    committer: {
-                        name: 'ATLauncher Bot',
-                        email: '+atlauncher-bot@users.noreply.github.com',
-                    },
-                    branch: 'master',
-                });
-            } catch (e) {
-                if (e.code === 404) {
-                    console.log(`File '${templateFile}' already deleted for project ${owner}/${repo}`);
-                    return;
-                }
-
-                throw e;
-            }
-        });
-    } catch (e) {
-        console.log('-----');
-        console.error(`[${owner}/${repo}] Error from GitHub:`);
-        console.log();
-        console.error(e);
-        process.exit(1);
-    }
-});
+            });
+        } catch (e) {
+            console.log('-----');
+            console.error(`[${owner}/${repo}] Error from GitHub:`);
+            console.log();
+            console.error(e);
+            process.exit(1);
+        }
+    });
